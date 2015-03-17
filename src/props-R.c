051f7ddb19bdda0fc61e3a8e1e68415fe6306f33
Rodrigo Canales <rocanale@gmail.com>
2015-03-17 17:41:32 +0100

    Adding matrix norms for complex matrices

--- src/props-R.c
+++ src/props-R.c
@@ -7,6 +7,13 @@ SEXP frobeniusNorm_d(SEXP RptrA){
   return ans;
 }
 
+SEXP frobeniusNorm_z(SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElFrobeniusNorm_z( toMatrix_z(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
 SEXP frobeniusNormDist_d(SEXP RptrA){
   SEXP ans = PROTECT( allocVector(REALSXP, 1) );
   ElFrobeniusNormDist_d( toDistMatrix_d(RptrA), REAL(ans) );
@@ -14,9 +21,23 @@ SEXP frobeniusNormDist_d(SEXP RptrA){
   return ans;
 }
 
+SEXP frobeniusNormDist_z(SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElFrobeniusNormDist_z( toDistMatrix_z(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
 SEXP symmetricFrobeniusNorm_d(SEXP uplo, SEXP RptrA){
   SEXP ans = PROTECT( allocVector(REALSXP, 1) );
   ElSymmetricFrobeniusNorm_d( parseUpLo(uplo), toMatrix_d(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP symmetricFrobeniusNorm_z(SEXP uplo, SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElSymmetricFrobeniusNorm_z( parseUpLo(uplo), toMatrix_z(RptrA), REAL(ans) );
   UNPROTECT(1);
   return ans;
 }
@@ -29,10 +50,25 @@ SEXP symmetricFrobeniusNormDist_d(SEXP uplo, SEXP RptrA){
   return ans;
 }
 
+SEXP symmetricFrobeniusNormDist_z(SEXP uplo, SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElSymmetricFrobeniusNormDist_z( parseUpLo(uplo), toDistMatrix_z(RptrA),
+                                  REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
 
 SEXP infinityNorm_d(SEXP RptrA){
   SEXP ans = PROTECT( allocVector(REALSXP, 1) );
   ElInfinityNorm_d( toMatrix_d(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
+
+SEXP infinityNorm_z(SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElInfinityNorm_z( toMatrix_z(RptrA), REAL(ans) );
   UNPROTECT(1);
   return ans;
 }
@@ -44,9 +80,23 @@ SEXP infinityNormDist_d(SEXP RptrA){
   return ans;
 }
 
+SEXP infinityNormDist_z(SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElInfinityNormDist_z( toDistMatrix_z(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
 SEXP symmetricInfinityNorm_d(SEXP uplo, SEXP RptrA){
   SEXP ans = PROTECT( allocVector(REALSXP, 1) );
   ElSymmetricInfinityNorm_d( parseUpLo(uplo), toMatrix_d(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP symmetricInfinityNorm_z(SEXP uplo, SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElSymmetricInfinityNorm_z( parseUpLo(uplo), toMatrix_z(RptrA), REAL(ans) );
   UNPROTECT(1);
   return ans;
 }
@@ -59,6 +109,13 @@ SEXP symmetricInfinityNormDist_d(SEXP uplo, SEXP RptrA){
   return ans;
 }
 
+SEXP symmetricInfinityNormDist_z(SEXP uplo, SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElSymmetricInfinityNormDist_z( parseUpLo(uplo), toDistMatrix_z(RptrA),
+                                 REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
 
 SEXP maxNorm_d(SEXP RptrA){
   SEXP ans = PROTECT( allocVector(REALSXP, 1) );
@@ -67,9 +124,24 @@ SEXP maxNorm_d(SEXP RptrA){
   return ans;
 }
 
+
+SEXP maxNorm_z(SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElMaxNorm_z( toMatrix_z(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
 SEXP maxNormDist_d(SEXP RptrA){
   SEXP ans = PROTECT( allocVector(REALSXP, 1) );
   ElMaxNormDist_d( toDistMatrix_d(RptrA), REAL(ans) );
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP maxNormDist_z(SEXP RptrA){
+  SEXP ans = PROTECT( allocVector(REALSXP, 1) );
+  ElMaxNormDist_z( toDistMatrix_z(RptrA), REAL(ans) );
   UNPROTECT(1);
   return ans;
 }
